2de8faff8db934e305d821cf990e4feeb751b536
nGyu <vpdls1511@gmail.com>
2023-04-06 11:11:10 +0900

    feat(vpdls1511) : 달력 추가

--- src/components/molecules/calendar/StandardCalendar.js
+++ src/components/molecules/calendar/StandardCalendar.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import Calendar from "react-calendar";
+import moment from "moment";
 import {useState} from "react";
 
 const StandardCalenderContainer = styled.div`
   width: 100%;
   height: 100%;
+
   .react-calendar {
     position: relative;
     width: 100%;
@@ -13,56 +15,91 @@ const StandardCalenderContainer = styled.div`
     color: #222;
     border-radius: 8px;
     box-shadow: 0 12px 24px rgba(0, 0, 0, 0.2);
-    font-family: Arial, Helvetica, sans-serif;
     line-height: 1.125em;
   }
-  .react-calendar__navigation__label{
+  .react-calendar__navigation{
+    padding: 20px;
+  }
+
+  .react-calendar__navigation__label {
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 600;
     font-size: 18px;
   }
+
   .react-calendar__navigation__prev-button,
-  .react-calendar__navigation__next-button{
+  .react-calendar__navigation__next-button {
     position: absolute;
+    transform: scale(2);
+    color: #555555;
   }
-  .react-calendar__navigation__prev-button{
-    right: 20px;
+
+  .react-calendar__navigation__prev-button {
+    right: 6rem;
   }
-  .react-calendar__navigation__next-button{
-    right: 10px;
+
+  .react-calendar__navigation__next-button {
+    right: 3rem;
   }
-  
+
   .react-calendar__navigation__prev2-button,
-  .react-calendar__navigation__next2-button{
+  .react-calendar__navigation__next2-button {
     display: none;
   }
+
   .react-calendar__viewContainer,
-  .react-calendar__month-view{
+  .react-calendar__month-view {
     width: 100%;
     height: 90%;
   }
-  .react-calendar__month-view{
-    >div{
-      width: 100%;
-      height: 100%;
-      >div{
-        width: 100%;
-        height: 100%;
-        div.react-calendar__month-view__days{
-          width: 100%;
-          height: 100%;
-        }
-      }
-      
-    }
+  
+  .react-calendar__month-view__days__day,
+  .react-calendar__month-view__weekdays__weekday,
+  .react-calendar__month-view__days__day--neighboringMonth {
+    font-family: 'Montserrat';
+    font-style: normal;
+    text-align: center;
+    font-weight: 600;
+    font-size: 15px;
+    color: #555555;
   }
+  
+  .react-calendar__tile{
+    margin: 5px 0;
+    padding: 10px;
+  }
+  
+  .react-calendar__month-view__weekdays__weekday,
+  .react-calendar__month-view__days__day--neighboringMonth {
+    color: #9DA2AE;
+  }
+  .react-calendar__month-view__weekdays{
+    > div > abbr { text-decoration: none; }
+  }
+  .react-calendar__tile--range{
+    background: #EFF2FF;
+  }
+  .react-calendar__tile--rangeStart,
+  .react-calendar__tile--rangeEnd{
+    border-radius: 10px;
+    background: #0129FF;
+    color: white;
+  }
+  
+
 `
 const StandardCalendar = () => {
   const [date, setDate] = useState(new Date())
 
-  return<StandardCalenderContainer>
-    <Calendar onChange={setDate} value={date}/>
+  return <StandardCalenderContainer>
+    <Calendar
+      onChange={setDate}
+      value={date}
+      formatDay={(locale, date) => moment(date).format("DD")}
+      selectRange={true}
+      locale={'kr'}
+    />
   </StandardCalenderContainer>
 }
 
