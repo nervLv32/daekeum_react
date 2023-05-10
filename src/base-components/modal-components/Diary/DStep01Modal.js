726c4f812ecdbfb09d7bafa37e4df5837e6dc8cd
leejs7 <wtcw27@gmail.com>
2023-05-10 13:36:02 +0900

    [Feat] 일지작성 1 작업완료

--- src/base-components/modal-components/Diary/DStep01Modal.js
+++ src/base-components/modal-components/Diary/DStep01Modal.js
@@ -8,8 +8,8 @@ const ModalWrap = styled.div`
   max-height: 70vh;
   overflow-y: auto;
   background-color: #fff;
-  box-shadow: 10px -4px 10px rgba(0, 0, 0, 0.1);
-  border-radius: 20px 20px 0px 0px;
+  box-shadow: 1rem -0.4rem 1rem rgba(0, 0, 0, 0.1);
+  border-radius: 2rem 2rem 0px 0px;
   .title {
     padding: 1.5rem 0;
     text-align: center;
@@ -21,7 +21,7 @@ const ModalWrap = styled.div`
   }
   .step-list {
     background-color: #f7f7f7;
-    padding: 15px 30px;
+    padding: 1.5rem 3rem;
     ul {
       width: 100%;
       height: auto;
@@ -33,7 +33,7 @@ const ModalWrap = styled.div`
         content: '';
         display: block;
         width: 100%;
-        height: 1px;
+        height: 0.1rem;
         background-color: #9da2ae;
         position: absolute;
         left: 0;
@@ -46,22 +46,22 @@ const ModalWrap = styled.div`
         &::before {
           content: '';
           display: block;
-          width: 8px;
-          height: 3px;
+          width: 0.8rem;
+          height: 0.3rem;
           background-color: #f7f7f7;
           position: absolute;
-          right: -8px;
+          right: -0.8rem;
           top: 50%;
           transform: translateY(-50%);
         }
         &::after {
           content: '';
           display: block;
-          width: 8px;
-          height: 3px;
+          width: 0.8rem;
+          height: 0.3rem;
           background-color: #f7f7f7;
           position: absolute;
-          left: -8px;
+          left: -0.8rem;
           top: 50%;
           transform: translateY(-50%);
         }
@@ -79,18 +79,18 @@ const ModalWrap = styled.div`
           &.on {
             box-sizing: content-box;
             background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #0129FF;
-            border: 2px solid #A9B5FF;
+            border: 0.2rem solid #A9B5FF;
           }
         }
       }
     }
   }
   .modal-body {
-    padding: 30px 30px 25px;
-    dl {
+    padding: 3rem 3rem 2.5rem;
+    .input-info {
       width: 100%;
       height: auto;
-      &:not(:last-child) {
+      &:not(:last-of-type) {
         margin-bottom: 1rem;
       }
       dt {
@@ -106,14 +106,95 @@ const ModalWrap = styled.div`
           width: 100%;
           height: 3.3rem;
           background: #fff;
-          border: 1px solid #8885CB;
-          border-radius: 10px;
+          border: 0.1rem solid #8885CB;
+          border-radius: 1rem;
           font-size: 1.2rem;
           font-weight: 500;
           color: #1c1b1f;
           padding: 0 1.5rem;
+          &.bg {
+            background: #EFF2FF;
+          }
+          &::placeholder {
+            font-size: 1.2rem;
+            font-weight: 500;
+            color: #9da2ae;
+          }
+        }
+        label {
+          display: block;
+          position: relative;
+          input {
+            padding-right: 10.8rem;
+          }
+          button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 9.3rem;
+            height: 3.3rem;
+            position: absolute;
+            right: 0;
+            top: 0;
+            background: #5A55CA;
+            border: 0.1rem solid #8885CB;
+            border-radius: 1rem;
+            font-size: 1.2rem;
+            font-weight: 600;
+            color: #fff;
+            img {
+              display: block;
+              width: 1.6rem;
+              margin-right: 0.4rem;
+            }
+          }
         }
       }
+    }
+    .product-info {
+      width: 100%;
+      height: auto;
+      background: #F7F7F7;
+      border: 0.1rem solid #CACCD1;
+      border-radius: 1rem;
+      padding: 2rem 1.7rem;
+      margin-top: 1rem;
+      dl {
+        display: flex;
+        align-items: center;
+        &:not(:last-child) {
+          margin-bottom: 1.4rem;
+        }
+        dt {
+          width: 9rem;
+          padding-right: 1.2rem;
+          font-size: 1.2rem;
+          line-height: 1.2rem;
+          font-weight: 500;
+          color: #555;
+        }
+        dd {
+          width: calc(100% - 9rem);
+          height: 1.2rem;
+          border-bottom: 0.1rem solid #9DA2AE;
+        }
+      }
+    }
+  }
+  .btn-wrap {
+    width: 100%;
+    height: auto;
+    padding: 1.7rem 3rem;
+    background: #F7F7F7;
+    border-radius: 2rem 2rem 0px 0px;
+    button {
+      width: 100%;
+      height: 3.4rem;
+      background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #0129FF;
+      border-radius: 1rem;
+      font-size: 1.4rem;
+      font-weight: 700;
+      color: #f7f7f7;
     }
   }
 `
@@ -150,24 +231,57 @@ const DStep01Modal = () => {
         </ul>
       </div>
       <div className="modal-body">
-        <dl>
+        <dl className="input-info">
           <dt className="essential">일지번호</dt>
           <dd>
-            <input type="text" />
+            <input type="text" className="bg" />
           </dd>
         </dl>
-        <dl>
+        <dl className="input-info">
           <dt className="essential">장비정보</dt>
           <dd>
             <label>
-              <input type="text" />
+              <input type="text" placeholder="해당업체 장비를 검색하세요." />
               <button type="button">
-                <i></i>
+                <img src="../icons/search-icon.png" alt="검색 아이콘" />
                 <span>장비검색</span>
               </button>
             </label>
           </dd>
         </dl>
+        <div className="product-info">
+          <dl>
+            <dt>MODEL-NO</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>수조</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>박스</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>MFG-NO</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>사용전압</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>설치방향</dt>
+            <dd></dd>
+          </dl>
+          <dl>
+            <dt>침전제</dt>
+            <dd></dd>
+          </dl>
+        </div>
+      </div>
+      <div className="btn-wrap">
+        <button type="button">다음</button>
       </div>
     </ModalWrap>
   )
