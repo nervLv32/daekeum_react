8a96d8d834e1cfe8860fd45cbc55b43d38d01cb9
nGyu <vpdls1511@gmail.com>
2023-07-14 15:31:58 +0900

    refactor(vpdls1511) : 상신_메인화면 더미데이터 삭제

--- src/pages/Report/index.js
+++ src/pages/Report/index.js
@@ -27,31 +27,6 @@ const TopSearchcMenuWrap = styled.ul`
 
 
 const Report = () => {
-
-  const dummyData = [
-    {
-      no: 1,
-      date: '2023-02-08',
-      company: '(주)대금지오웰',
-      manager: '정명길',
-      payState: '결재완료',
-      site: '호남고속도로 첨단방면 연결로 개설공사 2구간',
-      department: '수도권A',
-      regionFirst: '인천',
-      regionSecond: '미추홀구',
-    },
-    {
-      no: 2,
-      date: '2023-02-08',
-      company: '(주)대금지오웰',
-      manager: '정명길',
-      payState: '결재완료',
-      site: '호남고속도로 첨단방면 연결로 개설공사 2구간',
-      department: '수도권A',
-      regionFirst: '서울',
-      regionSecond: '동작구',
-    },
-  ];
   const [topMenu, setTopMenu] = useState(false);
 
   const {openModal} = useModal();
