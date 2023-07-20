9abd0110d703da5a355139db0936ba6ffba4c76d
leejs7 <wtcw27@gmail.com>
2023-07-20 13:29:48 +0900

    [Feat] 입고서류상신 모달 작업 step01, step02, step03 작업완료

--- src/base-components/modal-components/report/documentModal/Case03/RPCase0301Modal.js
+++ src/base-components/modal-components/report/documentModal/Case03/RPCase0301Modal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import styled from "styled-components";
 import RPModalBody from "../../../../../components/report/RPModalBody";
 import RPModalBottom from "../../../../../components/report/RPModalListBottom";
@@ -7,6 +7,9 @@ import RPModalListTop from "../../../../../components/report/RPModalListTop";
 import RPModalSearch from "../../../../../components/report/RPModalSearch";
 import RPModalTop from "../../../../../components/report/RPModalTop";
 import { useModal } from "../../../../../hooks/useModal";
+import fetchService from '../../../../../util/fetchService';
+import {useRecoilState, useRecoilValue} from 'recoil';
+import {firstExportDocument, reportAtom, reportParamAtom} from '../../../../../recoil/reportAtom';
 
 import RPCase0302Modal from "./RPCase0302Modal";
 
@@ -18,114 +21,68 @@ const RPCase0301ModalWrap = styled.div`
 
 const RPCase0301Modal = () => {
   const { openModal, closeModal } = useModal();
-
-  const dummyData = [
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-    {
-      company: "(주)대금지오웰",
-      ceo: "이승우",
-      companyNum: "131-81-19404",
-      sector: "제조업",
-      category: "산업기계외",
-      address: "인천광역시 미추홀구 장고개로92번길38(도화동)",
-      manager: "정명길",
-      managerPhone: "010-1234-5678"
-    },
-  ]
+  const firstExport = useRecoilValue(firstExportDocument)
 
   const modalData = {
     title: 'RPCase0302Modal',
     callback: () => alert('Modal Callback()'),
   };
+
+  const observeTargetRef = useRef(null);
+  const [isLoading, setLoading] = useState(false);
+  const [reports, setReports] = useState([]);
+  const [params, setParams] = useState({
+    searchword: '',
+    currentPage: '1',
+    pageSize: '10',
+  });
+
+  const changeParam = (key, value) => {
+    setParams({
+      ...params,
+      currentPage: '1',
+      [key] : value
+    })
+  };
+
+  const onIntersect = new IntersectionObserver(([entry], observer) => {
+    if (entry.isIntersecting) {
+      setLoading(true);
+      setParams({
+        ...params,
+        currentPage: parseInt(params.currentPage) + 1,
+      });
+    }
+  });
+
+  const fetchList = (list) => {
+    fetchService('/approval/clientList', 'post', params)
+      .then((res) => {
+        const data = [...list, ...res.data];
+        setReports(data);
+        if (res.data.length > 9) {
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+        }
+      });
+  };
+
+  useEffect(() => {
+    if(parseInt(params.currentPage) > 1) {
+      fetchList(reports);
+    }
+  }, [params.currentPage])
+
+  useEffect(() => {
+    fetchList([]);
+    setLoading(true)
+  }, [params.searchword])
+
+  useEffect(() => {
+    !isLoading ? onIntersect.observe(observeTargetRef.current) : onIntersect.disconnect();
+    return () => onIntersect.disconnect();
+  }, [isLoading])
 
   /******* 입출고 서류상신 - 입고요청서 case 03의 첫 번째 스텝 *******/
   return <RPCase0301ModalWrap>
@@ -134,10 +91,11 @@ const RPCase0301Modal = () => {
     <RPModalBody>
       <RPModalListTop type="type01" dep1="업체명" dep2="대표자" dep3="사업자번호" />
       {
-        dummyData.map((item, idx) => {
+        reports?.length > 0 && reports.map((item, idx) => {
           return <RPModalListItem item={item} key={idx} type="type01"/>
         })
       }
+      <div ref={observeTargetRef}/>
     </RPModalBody>
     <RPModalBottom>
       <button className="primary-btn" onClick={() => {
--- src/base-components/modal-components/report/documentModal/Case03/RPCase0303Modal.js
+++ src/base-components/modal-components/report/documentModal/Case03/RPCase0303Modal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import styled from "styled-components";
 import RPModalBody from "../../../../../components/report/RPModalBody";
 import RPModalBottom from "../../../../../components/report/RPModalListBottom";
@@ -11,6 +11,10 @@ import { useModal } from "../../../../../hooks/useModal";
 import RPCase0302Modal from "./RPCase0302Modal";
 import RPC03Step01Modal from "../../stepModal/Case03/RPC03Step01Modal";
 
+import fetchService from '../../../../../util/fetchService';
+import {useRecoilState} from 'recoil';
+import {firstExportDocument} from '../../../../../recoil/reportAtom';
+
 
 const RPCase0303ModalWrap = styled.div`
   background-color: #fff;
@@ -20,52 +24,84 @@ const RPCase0303ModalWrap = styled.div`
 const RPCase0303Modal = () => {
 
   const { openModal, closeModal } = useModal();
-
-  const dummyData = [
-    {
-      division: "회수",
-      dkno: "15222",
-      mcno: "F003",
-      model: "R10DN",
-      operation: "회수 가동",
-      mif: "MIF\n일치",
-      startDate: "2023-01-01",
-      endDate: "2023-02-09",
-      defaultMonth: "30",
-      changeMonth: "30",
-      price: "14,000,000",
-    },
-    {
-      division: "회수",
-      dkno: "15222",
-      mcno: "F003",
-      model: "R10DN",
-      operation: "회수 가동",
-      mif: "MIF\n일치",
-      startDate: "2023-01-01",
-      endDate: "2023-02-09",
-      defaultMonth: "30",
-      changeMonth: "30",
-      price: "14,000,000",
-    },
-  ]
+  const [firstExport, setFirstExport] = useRecoilState(firstExportDocument)
 
   const modalData = {
     title: 'RPDoc Modal Modal',
     callback: () => alert('Modal Callback()'),
   };
 
+  const observeTargetRef = useRef(null);
+  const [isLoading, setLoading] = useState(false);
+  const [equip, setEquip] = useState([]);
+  const [params, setParams] = useState({
+    searchword: '',
+    pageSize: '10',
+    currentPage: '1',
+    거래처코드 : firstExport.client.거래처코드,
+    현장코드: firstExport.site.현장코드
+  });
+
+  const changeParam = (key, value) => {
+    setParams({
+      ...params,
+      currentPage: '1',
+      [key] : value
+    })
+  };
+
+  const onIntersect = new IntersectionObserver(([entry], observer) => {
+    if (entry.isIntersecting) {
+      setLoading(true);
+      setParams({
+        ...params,
+        currentPage: parseInt(params.currentPage) + 1,
+      });
+    }
+  });
+
+  const fetchList = (list) => {
+    fetchService('/approval/equiplist', 'post', params)
+      .then((res) => {
+        console.log(res)
+        const data = [...list, ...res.data];
+        setEquip(data);
+        if (res.data.length > 9) {
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+        }
+      });
+  };
+
+  useEffect(() => {
+    if(parseInt(params.currentPage) > 1) {
+      fetchList(equip);
+    }
+  }, [params.currentPage])
+
+  useEffect(() => {
+    fetchList([]);
+    setLoading(true)
+  }, [params.searchword])
+
+  useEffect(() => {
+    !isLoading ? onIntersect.observe(observeTargetRef.current) : onIntersect.disconnect();
+    return () => onIntersect.disconnect();
+  }, [isLoading])
+
   /******* 입출고 서류상신 - 입고요청서 case 03의 세 번째 스텝 *******/
   return <RPCase0303ModalWrap>
     <RPModalTop title="입고서류상신" />
-    <RPModalSearch dep1="업체명" dep2="현장명" dep3="장비정보" />
+    <RPModalSearch dep1="업체명" dep2="현장명" dep3="장비정보" changeParam={changeParam} />
     <RPModalBody>
       <RPModalListTop type="type03" dep1="구분" dep2="DKNO" dep3="MCNO" dep4="기종" dep5="가동구분" dep6="MIF" />
       {
-        dummyData.map((item, idx) => {
+        equip?.length > 0 && equip.map((item, idx) => {
           return <RPModalListItem item={item} key={idx} type="type03" />
         })
       }
+      <div ref={observeTargetRef}/>
     </RPModalBody>
     <RPModalBottom>
       <button className="primary-btn" onClick={() => {
