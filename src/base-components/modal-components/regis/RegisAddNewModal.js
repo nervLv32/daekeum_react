60f3a8db881b76103ef58a8299a6278eea774e9f
nGyu <vpdls1511@gmail.com>
2023-08-01 16:31:09 +0900

    fix(vpdls1511) : 통합등록  - 현장등록 / 수정 완료

--- src/base-components/modal-components/regis/RegisAddNewModal.js
+++ src/base-components/modal-components/regis/RegisAddNewModal.js
@@ -1,17 +1,17 @@
-import React, {useEffect, useState} from "react";
-import styled from "styled-components";
-import OrderStateBtn from "../../../components/atom/OrderStateBtn";
-import { useModal } from "../../../hooks/useModal";
-import {useRecoilState} from "recoil";
-import {regisAtom} from "../../../recoil/regisAtom";
-import userAtom from "../../../recoil/userAtom";
-import fetchService from "../../../util/fetchService";
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components'
+import {useModal} from '../../../hooks/useModal'
+import {useRecoilState} from 'recoil'
+import {regisAtom} from '../../../recoil/regisAtom'
+import userAtom from '../../../recoil/userAtom'
+import fetchService from '../../../util/fetchService'
 import ConfirmAlert from '../ConfirmAlert'
 
 const RegisAddNewModalWrap = styled.div`
   max-height: 70vh;
   overflow-y: scroll;
   width: 100%;
+
   .modal-top {
     border-radius: 20px 20px 0 0;
     background-color: #fff;
@@ -19,13 +19,15 @@ const RegisAddNewModalWrap = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    border-bottom :1px solid #e9e9e9;
+    border-bottom: 1px solid #e9e9e9;
+
     .title {
       font-weight: 700;
       font-size: 16px;
       color: #1c1b1f;
     }
   }
+
   .modal-body {
     padding: 25px 30px;
     padding-bottom: 70px;
@@ -37,27 +39,34 @@ const InputList = styled.ul`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+
   li {
     &:not(:last-child) {
       margin-bottom: 10px;
     }
+
     &.full {
       width: 100%;
     }
+
     &.required {
       input {
         background-color: #EFF2FF;
       }
     }
+
     width: calc(50% - 4px);
+
     p {
       font-weight: 500;
       font-size: 11px;
       color: #1c1b1f;
       margin-bottom: 4px;
-      display : inline-block;
+      display: inline-block;
+
       &.red-point {
         position: relative;
+
         &:after {
           content: '';
           display: block;
@@ -71,6 +80,7 @@ const InputList = styled.ul`
         }
       }
     }
+
     input {
       width: 100%;
       box-sizing: border-box;
@@ -81,10 +91,12 @@ const InputList = styled.ul`
       border-radius: 10px;
       font-family: var(--font-mont);
       color: #1c1b1f;
+
       &::placeholder {
         color: #9DA2AE;
       }
     }
+
     textarea {
       width: 100%;
       box-sizing: border-box;
@@ -96,6 +108,7 @@ const InputList = styled.ul`
       border-radius: 10px;
       font-family: var(--font-mont);
       color: #1c1b1f;
+
       &::placeholder {
         color: #9DA2AE;
       }
@@ -115,22 +128,26 @@ const ModalBtm = styled.div`
   left: 0;
   width: 100%;
   z-index: 10;
+
   & > *:not(:last-child) {
-      margin-right: 10px;
-    }
+    margin-right: 10px;
+  }
+
   > button {
     cursor: pointer;
     width: calc(50% - 5px);
   }
+
   .primary-btn {
     height: 34px;
     padding: 0 30px;
     font-size: 14px;
     font-weight: 700;
-    background : linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #0129FF;
+    background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #0129FF;
     border-radius: 10px;
     color: #fff;
   }
+
   .del-btn {
     padding: 0 15px;
     height: 34px;
@@ -147,17 +164,16 @@ const ModalBtm = styled.div`
 `
 
 const RegisAddNewModal = ({no}) => {
-  const { closeModal, openModal } = useModal();
+  const {closeModal, openModal} = useModal()
 
   const modalData = {
     title: 'ConfirmAlert',
     callback: () => alert('Modal Callback()'),
-  };
+  }
 
   const [regis, setRegis] = useRecoilState(regisAtom)
   const [user, setUser] = useRecoilState(userAtom)
   const [edit, setEdit] = useState({
-    거래처코드: '',
     업체명: '',
     대표자성명: '',
     업태: '',
@@ -172,19 +188,36 @@ const RegisAddNewModal = ({no}) => {
     주소: '',
     고객분류: '',
     거래처분류: '',
-    결제구분: '',
+    결제구분: null,
     최종수정자: user.auth.한글이름,
   })
 
   const updateEdit = (key, value) => {
     setEdit({
       ...edit,
-      [key] : value
+      [key]: value,
     })
   }
 
+  const checkEdit = () => {
+    let flag = true
+    Object.entries(edit).map(([key, value]) => {
+      if(key === '전화번호1') {
+        setEdit({
+          ...edit,
+          '전화번호': value
+        })
+      }
+      if (key !== '결제구분' && !value){
+        console.log(key,value)
+        flag = false
+      }
+    })
+    return flag
+  }
+
   const updateRegis = () => {
-    fetchService(`/enroll/${no === -1 ? 'clientAdd' : 'clientUpdate'}`, 'post',edit)
+    fetchService(`/enroll/${no === -1 ? 'clientAdd' : 'clientUpdate'}`, 'post', edit)
       .then((res) => {
         console.log(res.data)
         closeModal()
@@ -193,147 +226,155 @@ const RegisAddNewModal = ({no}) => {
   }
 
   useEffect(() => {
-    if(no !== -1){
+    if (no !== -1) {
       setEdit({
         ...edit,
-        ...regis.filter(it => it.rownum === no)[0]
+        ...regis.filter(it => it.rownum === no)[0],
       })
     }
   }, [])
 
   return (
     <RegisAddNewModalWrap>
-      <div className="modal-top">
-        <h6 className="title">{no !== -1 ? '업체수정' : '신규업체등록'}</h6>
+      <div className='modal-top'>
+        <h6 className='title'>{no !== -1 ? '업체수정' : '신규업체등록'}</h6>
       </div>
-      <div className="modal-body">
+      <div className='modal-body'>
         <InputList>
-          <li className="full required">
-            <p>거래처코드</p>
-            <input type="text"
-                   placeholder="거래처코드를 입력하세요"
-                   value={edit.거래처코드 ? edit.거래처코드 : ''}
-                   readOnly={true}
-                   disabled={true}
-            />
-          </li>
+          {
+            no !== -1 && <li className='full required'>
+              <p>거래처코드</p>
+              <input type='text'
+                     placeholder='거래처코드를 입력하세요'
+                     value={edit.거래처코드 ? edit.거래처코드 : ''}
+                     readOnly={true}
+                     disabled={true}
+              />
+            </li>
+          }
           <li>
-            <p className="red-point">업체명</p>
-            <input type="text"
-                   placeholder="업체명을 입력하세요"
+            <p className='red-point'>업체명</p>
+            <input type='text'
+                   placeholder='업체명을 입력하세요'
                    value={edit.업체명 ? edit.업체명 : ''}
-                   onChange={(e) => updateEdit('업체명',e.target.value)}
+                   onChange={(e) => updateEdit('업체명', e.target.value)}
             />
           </li>
           <li>
-            <p className="red-point">대표자</p>
+            <p className='red-point'>대표자</p>
             <input
-              type="text"
-              placeholder="대표자를 입력하세요"
+              type='text'
+              placeholder='대표자를 입력하세요'
               value={edit.대표자성명 ? edit.대표자성명 : ''}
-              onChange={(e) => updateEdit('대표자성명',e.target.value)}
+              onChange={(e) => updateEdit('대표자성명', e.target.value)}
             />
           </li>
           <li>
             <p>업태</p>
-            <input type="text"  placeholder="업태를 입력하세요"
+            <input type='text' placeholder='업태를 입력하세요'
                    value={edit.업태 ? edit.업태 : ''}
-                   onChange={(e) => updateEdit('업태',e.target.value)}/>
+                   onChange={(e) => updateEdit('업태', e.target.value)}/>
           </li>
           <li>
             <p>종목</p>
-            <input type="text"  placeholder="종목를 입력하세요"
+            <input type='text' placeholder='종목를 입력하세요'
                    value={edit.종목 ? edit.종목 : ''}
-                   onChange={(e) => updateEdit('종목',e.target.value)}/>
+                   onChange={(e) => updateEdit('종목', e.target.value)}/>
           </li>
-          <li className="full">
+          <li className='full'>
             <p>사업자번호</p>
-            <input type="text"  placeholder="사업자번호를 입력하세요"
+            <input type='text' placeholder='사업자번호를 입력하세요'
                    value={edit.사업자번호 ? edit.사업자번호 : ''}
-                   onChange={(e) => updateEdit('사업자번호',e.target.value)}/>
+                   onChange={(e) => updateEdit('사업자번호', e.target.value)}/>
           </li>
-          <li className="full">
+          <li className='full'>
             <p>부서</p>
-            <input type="text"  placeholder="부서를 입력하세요"
+            <input type='text' placeholder='부서를 입력하세요'
                    value={edit.부서 ? edit.부서 : ''}
-                   onChange={(e) => updateEdit('부서',e.target.value)}/>
+                   onChange={(e) => updateEdit('부서', e.target.value)}/>
           </li>
           <li>
             <p>담당자</p>
-            <input type="text"  placeholder="담당자를 입력하세요"
+            <input type='text' placeholder='담당자를 입력하세요'
                    value={edit.담당자 ? edit.담당자 : ''}
-                   onChange={(e) => updateEdit('담당자',e.target.value)}/>
+                   onChange={(e) => updateEdit('담당자', e.target.value)}/>
           </li>
           <li>
             <p>직위</p>
-            <input type="text"  placeholder="직위를 입력하세요"
+            <input type='text' placeholder='직위를 입력하세요'
                    value={edit.직위 ? edit.직위 : ''}
-                   onChange={(e) => updateEdit('직위',e.target.value)}/>
+                   onChange={(e) => updateEdit('직위', e.target.value)}/>
           </li>
           <li>
             <p>휴대폰</p>
-            <input type="text"  placeholder="휴대폰을 입력하세요"
+            <input type='text' placeholder='휴대폰을 입력하세요'
                    value={edit.휴대폰 ? edit.휴대폰 : ''}
-                   onChange={(e) => updateEdit('휴대폰',e.target.value)}/>
+                   onChange={(e) => updateEdit('휴대폰', e.target.value)}/>
           </li>
           <li>
             <p>이메일</p>
-            <input type="text"  placeholder="이메일을 입력하세요"
+            <input type='text' placeholder='이메일을 입력하세요'
                    value={edit.이메일 ? edit.이메일 : ''}
-                   onChange={(e) => updateEdit('이메일',e.target.value)}/>
+                   onChange={(e) => updateEdit('이메일', e.target.value)}/>
           </li>
           <li>
             <p>전화번호</p>
-            <input type="text"  placeholder="전화번호를 입력하세요"
+            <input type='text' placeholder='전화번호를 입력하세요'
                    value={edit.전화번호1 ? edit.전화번호1 : ''}
-                   onChange={(e) => updateEdit('전화번호1',e.target.value)}/>
+                   onChange={(e) => updateEdit('전화번호1', e.target.value)}/>
           </li>
           <li>
             <p>팩스번호</p>
-            <input type="text"  placeholder="팩스번호를 입력하세요"
+            <input type='text' placeholder='팩스번호를 입력하세요'
                    value={edit.팩스번호 ? edit.팩스번호 : ''}
-                   onChange={(e) => updateEdit('팩스번호',e.target.value)}/>
+                   onChange={(e) => updateEdit('팩스번호', e.target.value)}/>
           </li>
-          <li className="full">
-            <p>회사코드</p>
-            <input type="text"  placeholder="회사코드를 입력하세요"
-                   value={edit.회사코드 ? edit.회사코드 : ''}
-                   onChange={(e) => updateEdit('회사코드',e.target.value)}/>
+          <li className='full'>
+            <p>주소</p>
+            <input type='text' placeholder='주소를 입력하세요'
+                   value={edit.주소 ? edit.주소 : ''}
+                   onChange={(e) => updateEdit('주소', e.target.value)}/>
           </li>
           <li>
             <p>고객분류</p>
-            <input type="text"  placeholder="고객분류를 입력하세요"
+            <input type='text' placeholder='고객분류를 입력하세요'
                    value={edit.고객분류 ? edit.고객분류 : ''}
-                   onChange={(e) => updateEdit('고객분류',e.target.value)}/>
+                   onChange={(e) => updateEdit('고객분류', e.target.value)}/>
           </li>
           <li>
             <p>거래처분류</p>
-            <input type="text"  placeholder="거래처분류를 입력하세요"
+            <input type='text' placeholder='거래처분류를 입력하세요'
                    value={edit.거래처분류 ? edit.거래처분류 : ''}
-                   onChange={(e) => updateEdit('거래처분류',e.target.value)}/>
+                   onChange={(e) => updateEdit('거래처분류', e.target.value)}/>
           </li>
         </InputList>
       </div>
 
       <ModalBtm>
-        <button className="primary-btn" onClick={() => {
+        <button className='primary-btn' onClick={() => {
           // updateRegis()
-        // closeModal()
-        openModal({ ...modalData, content: <ConfirmAlert
-            client={edit.업체명}
-            site={''}
-            text={no !== -1 ? '수정' : '등록'}
-            submit={updateRegis}
-            cancel={closeModal}
-          /> })
-      }}>저장</button>
-       <button className="del-btn" onClick={() => {
-        closeModal()
-        // openModal({ ...modalData, content: <RPC01Step01Modal /> })
-      }}>취소</button>
+          // closeModal()
+          if(checkEdit()){
+            openModal({ ...modalData, content: <ConfirmAlert
+                client={edit.업체명}
+                site={''}
+                text={no !== -1 ? '수정' : '등록'}
+                submit={updateRegis}
+                cancel={closeModal}
+              /> })
+          }else {
+            alert('비어있는 항목이 존재합니다.')
+          }
+        }}>저장
+        </button>
+        <button className='del-btn' onClick={() => {
+          closeModal()
+          // openModal({ ...modalData, content: <RPC01Step01Modal /> })
+        }}>취소
+        </button>
       </ModalBtm>
     </RegisAddNewModalWrap>
   )
 }
 
-export default RegisAddNewModal;
+export default RegisAddNewModal
--- src/base-components/modal-components/regis/RegisAddPlaceModal.js
+++ src/base-components/modal-components/regis/RegisAddPlaceModal.js
@@ -1,15 +1,16 @@
-import React, {useEffect, useState} from "react";
-import styled from "styled-components";
-import OrderStateBtn from "../../../components/atom/OrderStateBtn";
-import { useModal } from "../../../hooks/useModal";
-import {useRecoilValue} from "recoil";
-import userAtom from "../../../recoil/userAtom";
-import fetchService from "../../../util/fetchService";
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components'
+import {useModal} from '../../../hooks/useModal'
+import {useRecoilState, useRecoilValue} from 'recoil'
+import userAtom from '../../../recoil/userAtom'
+import fetchService from '../../../util/fetchService'
+import {selectCompanyAtom} from '../../../recoil/regisAtom'
 
 const RegisAddPlaceModalWrap = styled.div`
   max-height: 70vh;
   overflow-y: scroll;
   width: 100%;
+
   .modal-top {
     border-radius: 20px 20px 0 0;
     background-color: #fff;
@@ -17,13 +18,15 @@ const RegisAddPlaceModalWrap = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    border-bottom :1px solid #e9e9e9;
+    border-bottom: 1px solid #e9e9e9;
+
     .title {
       font-weight: 700;
       font-size: 16px;
       color: #1c1b1f;
     }
   }
+
   .modal-body {
     padding: 25px 30px;
     padding-bottom: 70px;
@@ -35,27 +38,34 @@ const InputList = styled.ul`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+
   li {
     &:not(:last-child) {
       margin-bottom: 10px;
     }
+
     &.full {
       width: 100%;
     }
+
     &.required {
       input {
         background-color: #EFF2FF;
       }
     }
+
     width: calc(50% - 4px);
+
     p {
       font-weight: 500;
       font-size: 11px;
       color: #1c1b1f;
       margin-bottom: 4px;
-      display : inline-block;
+      display: inline-block;
+
       &.red-point {
         position: relative;
+
         &:after {
           content: '';
           display: block;
@@ -69,6 +79,7 @@ const InputList = styled.ul`
         }
       }
     }
+
     input {
       width: 100%;
       box-sizing: border-box;
@@ -79,10 +90,12 @@ const InputList = styled.ul`
       border-radius: 10px;
       font-family: var(--font-mont);
       color: #1c1b1f;
+
       &::placeholder {
         color: #9DA2AE;
       }
     }
+
     textarea {
       width: 100%;
       box-sizing: border-box;
@@ -94,6 +107,7 @@ const InputList = styled.ul`
       border-radius: 10px;
       font-family: var(--font-mont);
       color: #1c1b1f;
+
       &::placeholder {
         color: #9DA2AE;
       }
@@ -113,22 +127,26 @@ const ModalBtm = styled.div`
   left: 0;
   width: 100%;
   z-index: 10;
+
   & > *:not(:last-child) {
-      margin-right: 10px;
-    }
+    margin-right: 10px;
+  }
+
   > button {
     cursor: pointer;
     width: calc(50% - 5px);
   }
+
   .primary-btn {
     height: 34px;
     padding: 0 30px;
     font-size: 14px;
     font-weight: 700;
-    background : linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #0129FF;
+    background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #0129FF;
     border-radius: 10px;
     color: #fff;
   }
+
   .del-btn {
     padding: 0 15px;
     height: 34px;
@@ -145,28 +163,27 @@ const ModalBtm = styled.div`
 `
 
 const RegisAddPlaceModal = ({item}) => {
-  const { closeModal } = useModal();
+  const {closeModal} = useModal()
   const user = useRecoilValue(userAtom)
+  const [selectRegis, setSelectRegis] = useRecoilState(selectCompanyAtom)
   const [body, setBody] = useState(item ? item : {
-    거래처코드 : '',
-    현장코드: '',
-    현장명:'',
-    담당자:'',
-    직위:'',
-    휴대폰:'',
-    이메일 :'',
-    전화번호 :'',
-    팩스번호 :'',
-    주소 :'',
-    종료예정일 :'',
-    설치예정일 :'',
-    알림 :'',
-    고객분류 :'',
-    지역분류 :'',
-    현장분류 :'',
-    고객접점:'',
-    담당센터 :'',
-  });
+    현장명: '',
+    담당자: '',
+    직위: '',
+    휴대폰: '',
+    이메일: '',
+    전화번호: '',
+    팩스번호: '',
+    주소: '',
+    종료예정일: new Date(),
+    설치예정일: new Date(),
+    알림: '',
+    고객분류: '',
+    지역분류: '',
+    현장분류: '',
+    고객접점: '',
+    담당센터: '',
+  })
 
   const updateBody = (key, value) => {
     setBody({
@@ -176,7 +193,7 @@ const RegisAddPlaceModal = ({item}) => {
   }
 
   const submitBody = () => {
-    const url = `/enroll/${item ? 'siteAdd' : 'siteUpdate'}`
+    const url = `/enroll/${item ? 'siteUpdate' : 'siteAdd'}`
     console.log(body)
     fetchService(url, 'post', body).then(res => {
       console.log(res.data)
@@ -186,115 +203,126 @@ const RegisAddPlaceModal = ({item}) => {
   useEffect(() => {
     setBody({
       ...body,
-      UserInfo : user.auth
+      ...item,
+      거래처코드: selectRegis.client.code,
+      UserInfo: {
+        DeptCd: user.auth.부서코드,
+        DeptNm: user.auth.부서명,
+        EmpNm: user.auth.한글이름,
+        EmpNo: user.auth.사원코드,
+        회사코드: user.auth.회사코드,
+        DIV_CD: user.auth.DIV_CD,
+        usergwid: user.auth.usergwid,
+      },
     })
   }, [])
 
-
   return (
     <RegisAddPlaceModalWrap>
-      <div className="modal-top">
-        <h6 className="title">{item ? '현장수정' : '신규현장등록'}</h6>
+      <div className='modal-top'>
+        <h6 className='title'>{item ? '현장수정' : '신규현장등록'}</h6>
       </div>
-      <div className="modal-body">
+      <div className='modal-body'>
         <InputList>
-          <li className="full required">
-            <p>현장코드</p>
-            <input type="text"  placeholder="현장코드를 입력하세요" value={body.현장코드} readOnly={true}/>
-          </li>
-          <li className="full">
-            <p className="red-point">지역분류</p>
-            <input type="text"  placeholder="현장명을 입력하세요"
-                   value={body.지역분류} onChange={(e) => updateBody('지역분류', e.target.value)}
+          {
+            item && <li className='full required'>
+              <p>현장코드</p>
+              <input type='text' placeholder='현장코드를 입력하세요' value={body.현장코드} readOnly={true}/>
+            </li>
+          }
+          <li className='full'>
+            <p className='red-point'>현장명</p>
+            <input type='text' placeholder='현장명을 입력하세요'
+                   value={body.현장명} onChange={(e) => updateBody('현장명', e.target.value)}
             />
           </li>
           <li>
             <p>담당자</p>
-            <input type="text"  placeholder="담당자를 입력하세요"
+            <input type='text' placeholder='담당자를 입력하세요'
                    value={body.담당자} onChange={(e) => updateBody('담당자', e.target.value)}
             />
           </li>
           <li>
             <p>직위</p>
-            <input type="text"  placeholder="직위를 입력하세요"
+            <input type='text' placeholder='직위를 입력하세요'
                    value={body.직위} onChange={(e) => updateBody('직위', e.target.value)}
             />
           </li>
           <li>
             <p>휴대폰</p>
-            <input type="text"  placeholder="휴대폰을 입력하세요"
+            <input type='text' placeholder='휴대폰을 입력하세요'
                    value={body.휴대폰} onChange={(e) => updateBody('휴대폰', e.target.value)}
             />
           </li>
           <li>
             <p>이메일</p>
-            <input type="text"  placeholder="이메일을 입력하세요"
+            <input type='text' placeholder='이메일을 입력하세요'
                    value={body.이메일} onChange={(e) => updateBody('이메일', e.target.value)}
             />
           </li>
           <li>
             <p>전화번호</p>
-            <input type="text"  placeholder="전화번호를 입력하세요"
+            <input type='text' placeholder='전화번호를 입력하세요'
                    value={body.전화번호} onChange={(e) => updateBody('전화번호', e.target.value)}
             />
           </li>
           <li>
             <p>팩스번호</p>
-            <input type="text"  placeholder="팩스번호를 입력하세요"
+            <input type='text' placeholder='팩스번호를 입력하세요'
                    value={body.팩스번호} onChange={(e) => updateBody('팩스번호', e.target.value)}
             />
           </li>
-          <li className="full">
+          <li className='full'>
             <p>주소</p>
-            <input type="text"  placeholder="주소를 입력하세요"
+            <input type='text' placeholder='주소를 입력하세요'
                    value={body.주소} onChange={(e) => updateBody('주소', e.target.value)}
             />
           </li>
           <li>
             <p>종료예정일</p>
-            <input type="text"  placeholder="날짜를 입력하세요"
+            <input type='text' placeholder='날짜를 입력하세요'
                    value={body.종료예정일} onChange={(e) => updateBody('종료예정일', e.target.value)}
             />
           </li>
           <li>
             <p>설치예정일</p>
-            <input type="text"  placeholder="날짜를 입력하세요"
+            <input type='text' placeholder='날짜를 입력하세요'
                    value={body.설치예정일} onChange={(e) => updateBody('설치예정일', e.target.value)}
             />
           </li>
           <li>
             <p>알림</p>
-            <input type="text"  placeholder="알림을 입력하세요"
+            <input type='text' placeholder='알림을 입력하세요'
                    value={body.접속시알림 ? '승인' : '거부'} onChange={(e) => updateBody('접속시알림', e.target.value)}
             />
           </li>
           <li>
             <p>고객분류</p>
-            <input type="text"  placeholder="고객분류를 입력하세요"
+            <input type='text' placeholder='고객분류를 입력하세요'
                    value={body.고객분류} onChange={(e) => updateBody('고객분류', e.target.value)}
             />
           </li>
           <li>
-            <p className="red-point">지역분류</p>
-            <input type="text"  placeholder="지역분류를 입력하세요"
+            <p className='red-point'>지역분류</p>
+            <input type='text' placeholder='지역분류를 입력하세요'
                    value={body.지역분류} onChange={(e) => updateBody('지역분류', e.target.value)}
             />
           </li>
           <li>
-            <p className="red-point">현장분류</p>
-            <input type="text"  placeholder="현장분류를 입력하세요"
+            <p className='red-point'>현장분류</p>
+            <input type='text' placeholder='현장분류를 입력하세요'
                    value={body.현장분류} onChange={(e) => updateBody('현장분류', e.target.value)}
             />
           </li>
           <li>
             <p>고객접점</p>
-            <input type="text"  placeholder="고객접점을 입력하세요"
+            <input type='text' placeholder='고객접점을 입력하세요'
                    value={body.고객접점} onChange={(e) => updateBody('고객접점', e.target.value)}
             />
           </li>
           <li>
             <p>담당센터</p>
-            <input type="text"  placeholder="담당센터를 입력하세요"
+            <input type='text' placeholder='담당센터를 입력하세요'
                    value={body.담당센터} onChange={(e) => updateBody('담당센터', e.target.value)}
             />
           </li>
@@ -302,18 +330,20 @@ const RegisAddPlaceModal = ({item}) => {
       </div>
 
       <ModalBtm>
-        <button className="primary-btn" onClick={() => {
+        <button className='primary-btn' onClick={() => {
           submitBody()
-        closeModal()
-        // openModal({ ...modalData, content: <RPC01Step03Modal /> })
-      }}>저장</button>
-       <button className="del-btn" onClick={() => {
-        closeModal()
-        // openModal({ ...modalData, content: <RPC01Step01Modal /> })
-      }}>취소</button>
+          closeModal()
+          // openModal({ ...modalData, content: <RPC01Step03Modal /> })
+        }}>저장
+        </button>
+        <button className='del-btn' onClick={() => {
+          closeModal()
+          // openModal({ ...modalData, content: <RPC01Step01Modal /> })
+        }}>취소
+        </button>
       </ModalBtm>
     </RegisAddPlaceModalWrap>
   )
 }
 
-export default RegisAddPlaceModal;
+export default RegisAddPlaceModal
